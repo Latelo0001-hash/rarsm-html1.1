251d29951ff7fbcb784646181c0cca9f19471bef
Latelo0001-hash <latelo0001@gmail.com>
2026-08-13 16:11:02 +0100

    Améliore la hiérarchie des titres

--- istitutions-details.php
+++ istitutions-details.php
@@ -463,7 +463,7 @@ foreach ($institutions as $slug => $institution) {
 	<link rel="icon" href="favicon.png?v=20260702-favicon" type="image/png">
 	<link rel="shortcut icon" href="favicon.png?v=20260702-favicon" type="image/png">
 	<link rel="apple-touch-icon" href="favicon.png?v=20260702-favicon">
-	<link rel="stylesheet" href="css/site.css?v=20260813-accessibility-v22">
+	<link rel="stylesheet" href="css/site.css?v=20260813-headings-v23">
 	<script src="js/vendor/modernizr-2.6.2.min.js"></script>
 </head>
 <body>
